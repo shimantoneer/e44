6466b149c84ddc03b08c610d427eba0ff9d0d7b7
shimantoneer <poddernakla@gmail.com>
2018-06-06 02:27:17 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.alamo.com/en_US/car-rental/locations/us/memt71-memphis-international-airport.html?mcid=yext:245709");
 die();
 ?>
